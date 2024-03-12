a02e38ed55392ef1262b445f1739d5418e21105c
Samuel Hepditch <hepditchsam@gmail.com>
2024-03-12 11:42:29 -0400

    Initial Commit

---
+++ Harmonic Flight/Harmonic_FlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Harmonic_FlightApp.swift
+//  Harmonic Flight
+//
+//  Created by Sam Hepditch on 2024-03-12.
+//
+
+import SwiftUI
+
+@main
+struct Harmonic_FlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
